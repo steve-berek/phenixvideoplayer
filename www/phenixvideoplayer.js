6e69dc2c40ba0962bc087a6b8202d31a8e917354
steveberekoutou <dberekoutou@hiberus.com>
2020-02-06 17:53:58 +0100

    Clean project

--- www/phenixvideoplayer.js
+++ www/phenixvideoplayer.js
@@ -1,21 +1,54 @@
-// Empty constructor
-function PhenixVideoPlayer() {}
-
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-PhenixVideoPlayer.prototype.show = function(message, duration, successCallback, errorCallback) {
-  var options = {};
-  options.message = message;
-  options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'PhenixVideoPlayer', 'show', [options]);
-}
-
-// Installation constructor that binds ToastyPlugin to window
-PhenixVideoPlayer.install = function() {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-  window.plugins.phenixVideoPlayer = new PhenixVideoPlayer();
-  return window.plugins.phenixVideoPlayer;
-};
-cordova.addConstructor(PhenixVideoPlayer.install);
+cordova.define("cordova-plugin-phenixvideoplayer.PhenixVideoPlayer", function(require, exports, module) {
+    // Empty constructor
+    var exec = require('cordova/exec');
+    var  PhenixVideoPlayer = function(){};
+   
+   // The function that passes work along to native shells
+   // Message is a string, duration may be 'long' or 'short'
+   PhenixVideoPlayer.prototype.playVideo = function(videoPath, loop, controls, fullscreen, mute,  successCallback, errorCallback) {
+     var options = {};
+     options.videoPath = videoPath;
+     options.loop = loop;
+     options.controls = controls;
+     options.fullscreen = fullscreen;
+     options.mute = mute;
+     exec(successCallback, errorCallback, 'PhenixVideoPlayer', 'playVideo', [options]);
+   }
+   PhenixVideoPlayer.prototype.playMultipleVideos = function(videosPaths, loop, controls, fullscreen, mute,  successCallback, errorCallback) {
+     var options = {};
+     options.videosPaths = videosPaths;
+       options.loop = loop;
+       options.controls = controls;
+       options.fullscreen = fullscreen;
+       options.mute = mute;
+     exec(successCallback, errorCallback, 'PhenixVideoPlayer', 'playMultipleVideos', [options]);
+   }
+   PhenixVideoPlayer.prototype.stopVideo = function(successCallback, errorCallback) {
+     var options = {};
+     exec(successCallback, errorCallback, 'PhenixVideoPlayer', 'stopVideo', [options]);
+   }
+   
+   PhenixVideoPlayer.prototype.resetVideo = function(successCallback, errorCallback) {
+     var options = {};
+     exec(successCallback, errorCallback, 'PhenixVideoPlayer', 'resetVideo', [options]);
+   }
+   
+   module.exports = new PhenixVideoPlayer();
+   // Installation constructor that binds ToastyPlugin to window
+   PhenixVideoPlayer.install = function() {
+     if (!window.plugins) {
+       window.plugins = {};
+     }
+     window.plugins.phenixVideoPlayer = new PhenixVideoPlayer();
+     return window.plugins.phenixVideoPlayer;
+   };
+   cordova.addConstructor(PhenixVideoPlayer.install); 
+   
+   /* var exec = require("cordova/exec");
+   module.exports = {
+       playVideo: function (path, successCallback, errorCallback) {
+           exec(successCallback, errorCallback, "PhenixVideoPlayer", "playVideo", [path]);
+       }
+   }; */
+   });
+   
